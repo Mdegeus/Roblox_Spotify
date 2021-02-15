dc86675062c645dd19b996cb62da2feada8b18be
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-15 13:49:58 +0100

    Create finalTest2.lua

---
+++ finalTest2.lua
@@ -0,0 +1,5 @@
+{
+	"All Playlists":[{"Test List1", "Test List2"}],
+
+	"Test List1":[{{Name = "SUN MASS", Id = "1837461008"}, {Name = "Horrible keys", Id = "1837861894"}, {Name = "Chasing Lights", Id = "1842967125"}}]
+}
