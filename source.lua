c12ed8333d693e897e7d7ec1e44f615d4a081a54
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-16 19:25:51 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,7 +1,7 @@
 {
 "PlayLists": ["test1", "test2", "test3", "💙BTS💙"],
-"test1": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}],
-"test2": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}],
-"test3": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}],
-"💙BTS💙": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}]
+"test1": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+"test2": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+"test3": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+"💙BTS💙": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}]
 }
