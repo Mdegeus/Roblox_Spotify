57b33a15d66839c418d08befdcdfdfa53b3f0c6f
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-16 17:01:59 +0100

    Create lists.lua

---
+++ lists.lua
@@ -0,0 +1,6 @@
+{
+"PlayLists": ["test1", "test2", "test3"],
+"test1": [{"Name": ["Song1", Id: "636346346"]}, {"Name": ["Song2", Id: "636346346"]}, {"Name": ["Song3", Id: "636346346"]}],
+"test2": [{"Name": ["Song1", Id: "736346346"]}, {"Name": ["Song2", Id: "736346346"]}, {"Name": ["Song3", Id: "736346346"]}],
+"test3": [{"Name": ["Song1", Id: "836346346"]}, {"Name": ["Song2", Id: "836346346"]}, {"Name": ["Song3", Id: "836346346"]}]
+}
