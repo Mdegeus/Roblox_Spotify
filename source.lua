5a4b3820a013e7d62e30668745433866bb3e26f1
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-16 21:08:33 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,7 +1,15 @@
 {
 "PlayLists": ["test1", "test2", "test3", "💙BTS💙"],
-"test1": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
-"test2": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
-"test3": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
-"💙BTS💙": [{"Name": "The Truth Untold", "Id": "2554102535", "Speed": 0.9}, {"Name": "DNA", "Id": "5730351409", "Speed": 0.99}, {"Name": "Home", "Id": "3066271896", "Speed": 0.955}, {"Name": "Moon Jin", "Id": "4735280126", "Speed": 0.99}]
+
+  "test1": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+
+  "test2": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+
+  "test3": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+
+  "💙BTS💙": [{"Name": "The Truth Untold", 
+    "Id": "2554102535", "Speed": 0.9}, 
+  {"Name": "DNA", "Id": "5730351409", "Speed": 0.99}, 
+  {"Name": "Home", "Id": "3066271896", "Speed": 0.955}, 
+  {"Name": "Moon Jin", "Id": "4735280126", "Speed": 0.99}]
 }
