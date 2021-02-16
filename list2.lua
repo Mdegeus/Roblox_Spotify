3d044617248520f2d23e63df7944bf5e2e2e9f75
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-16 18:37:30 +0100

    Update list2.lua

--- list2.lua
+++ list2.lua
@@ -1,5 +1,5 @@
 {
-  "PlayLists": ["test1", "test2", "test3", "test4", "test5", "💙BTS💙"]
+  "PlayLists": ["test1", "test2", "test3", "test4", "test5", "💙BTS💙"],
   "test1": [{"Name": "Song1", "Id": "000", "Speed": "1"}, {"Name": "Song2", "Id": "000", "Speed": "1"}, {"Name": "Song3", "Id": "000", "Speed": "1"}],
   "test2": [{"Name": "Song1", "Id": "000", "Speed": "1"}, {"Name": "Song2", "Id": "000", "Speed": "1"}, {"Name": "Song3", "Id": "000", "Speed": "1"}],
   "test3": [{"Name": "Song1", "Id": "000", "Speed": "1"}, {"Name": "Song2", "Id": "000", "Speed": "1"}, {"Name": "Song3", "Id": "000", "Speed": "1"}],
