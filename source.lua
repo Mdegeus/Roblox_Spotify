52db49a446ad6d4e40f85657d8ff5675d651b678
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-16 19:11:18 +0100

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,7 @@
+{
+"PlayLists": ["test1", "test2", "test3", "💙BTS💙"],
+"test1": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
+"test2": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}],
+"test3": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}]
+"💙BTS💙": [{"Name": "Song1", "Id": "000"}, {"Name": "Song2", "Id": "000"}, {"Name": "Song3", "Id": "000"}]
+}
