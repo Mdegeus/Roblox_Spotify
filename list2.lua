52b1c19d1d5fb89a9b33e3148b4bd215f0d24de3
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-16 18:25:11 +0100

    Update list2.lua

--- list2.lua
+++ list2.lua
@@ -1,8 +1,11 @@
 {
-"PlayLists": ["test1", "test2", "test3"],
-"test1": [{"Name": ["Song1"], "Id": ["000"]}, {"Name": ["Song2"], "Id": ["000"]}, {"Name": ["Song3"], "Id": ["000"]}],
-"test2": [{"Name": ["Song1"], "Id": ["000"]}, {"Name": ["Song2"], "Id": ["000"]}, {"Name": ["Song3"], "Id": ["000"]}],
-"test3": [{"Name": ["Song1"], "Id": ["000"]}, {"Name": ["Song2"], "Id": ["000"]}, {"Name": ["Song3"], "Id": ["000"]}]
+  "PlayLists": ["test1", "test2", "test3", "test4", "test5", "💙BTS💙"]
+  "test1": [{"Name": ["Song1"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song2"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song3"], "Id": ["000"], "Speed": ["1"]}],
+  "test2": [{"Name": ["Song1"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song2"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song3"], "Id": ["000"], "Speed": ["1"]}],
+  "test3": [{"Name": ["Song1"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song2"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song3"], "Id": ["000"], "Speed": ["1"]}],
+  "test4": [{"Name": ["Song1"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song2"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song3"], "Id": ["000"], "Speed": ["1"]}],
+  "test5": [{"Name": ["Song1"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song2"], "Id": ["000"], "Speed": ["1"]}, {"Name": ["Song3"], "Id": ["000"], "Speed": ["1"]}],
+  "💙BTS💙": [{"Name": ["The Truth Untold"], "Id": ["2554102535"], "Speed": ["0.9"]}, {"Name": ["DNA"], "Id": ["5730351409"], "Speed": ["0.99"]}, {"Name": ["Home"], "Id": ["3066271896"], "Speed": ["0.955"]}, {"Name": ["Moon - Jin"], "Id": ["4735280126"], "Speed": ["0.99]}]
 }
 
 --------- test
