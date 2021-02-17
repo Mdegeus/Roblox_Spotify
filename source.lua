eef47a2610fa9cb50c559a265692ef0d5d26b959
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-02-17 14:57:54 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,5 +1,5 @@
 {
-"PlayLists": ["test1", "test2", "test3", "💙BTS💙"],
+"PlayLists": ["test1", "test2", "test3", "💙BTS💙", "Sub Urban"],
 
   "test1": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
 
@@ -7,9 +7,16 @@
 
   "test3": [{"Name": "Song1", "Id": "000", "Speed": 1}, {"Name": "Song2", "Id": "000", "Speed": 1}, {"Name": "Song3", "Id": "000", "Speed": 1}],
 
-  "💙BTS💙": [{"Name": "The Truth Untold", 
-    "Id": "2554102535", "Speed": 0.9}, 
-  {"Name": "DNA", "Id": "5730351409", "Speed": 0.99}, 
+  "💙BTS💙": [
+  {"Name": "The Truth Untold", "Id": "2554102535", "Speed": 0.9}, 
+  {"Name": "ON", "Id": "5730351409", "Speed": 0.99}, 
   {"Name": "Home", "Id": "3066271896", "Speed": 0.955}, 
-  {"Name": "Moon Jin", "Id": "4735280126", "Speed": 0.99}]
+  {"Name": "Moon Jin", "Id": "4735280126", "Speed": 0.99}
+  ],
+  
+  "Sub Urban": [
+  {"Name": "Cradles", "Id": "4738376177", "Speed": 1.15}, 
+  {"Name": "Broken", "Id": "5091658916", "Speed": 0.9}, 
+  {"Name": "Cirque", "Id": "5009777080", "Speed": 1.1}
+  ]
 }
